d8bee94adc0e990e1b86797dd5688edac34ac57a
ClieffordNOya <clieffordnoya28@gmail.com>
2024-11-18 01:15:14 +0800

    convert html to jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="container">
+  <div id="colspan">
+    <img src="image/fotosaya.jpg" className="img1" />
+    <h4>CLIEFFORD NOYA</h4>
+    <p><i className="fa fa-briefcase fa-fw large" />Web Developer</p>
+    <p><i className="fa fa-home fa-fw large" />Minahasa Utara, INDONESIA</p>
+    <p><i className="fa fa-phone fa-fw large" />089686272363</p>
+    <hr />
+    <h3><i className="fa fa-linux fa-fw large" />Skills</h3>
+    <p>HTML</p>
+    <div className="load">
+      <div className="progress" style={{width: '90%'}}>90%</div>
     </div>
+    <p>CSS</p>
+    <div className="load2">
+      <div className="progress2" style={{width: '80%'}}>80%</div>
+    </div>
+    <p>Java Script</p>
+    <div className="load3">
+      <div className="progress3" style={{width: '70%'}}>70%</div>
+    </div>
+    <p>Go Lang</p>
+    <div className="load4">
+      <div className="progress4" style={{width: '65%'}}>65%</div>
+    </div>
+    <p>Adobe Photoshop</p>
+    <div className="load4">
+      <div className="progress4" style={{width: '65%'}}>65%</div>
+    </div>
+    <p>Adobe Premiere</p>
+    <div className="load4">
+      <div className="progress4" style={{width: '65%'}}>65%</div>
+    </div>
+    <hr />
+    <h3><i className="fa fa-language fa-fw large" />Language</h3>
+    <p>English</p>
+    <p>Japanese</p>
+    <hr />
+    <h3><i className="fa fa-globe fa-fw large" />Social Media</h3>
+    <a href="#"><i className="fa fa-facebook fa-fw large" /></a>
+    <a href="#"><i className="fa fa-instagram fa-fw large" /></a>
+    <a href="#"><i className="fa fa-twitter fa-fw large" /></a>
+    <a href="#"><i className="fa fa-github fa-fw large" /></a>
+    <a href="#"><i className="fa fa-git fa-fw large" /></a>
+  </div>
+  <div className="container-padding">
+    <div className="padding">
+      <div className="content1">
+        <h2><i className="fa fa-briefcase fa-fw large" />School</h2>
+        <h3>SD Advent Salemba Jakarta</h3>
+        <p><i className="fa fa-calendar fa-fw large" />2008-2013</p>
+        <p>SD Advent Salemba Jakarta dengan nilai kelulusan 25.60
+        </p>
+      </div>
+      <hr />
+      <div className="content2">
+        <h3>SMP Advent Salemba Jakarta</h3>
+        <p><i className="fa fa-calendar fa-fw large" />2013-2016</p>
+        <p>SMP Advent Salemba Jakarta dengan nilai kelulusan 29.0
+        </p>
+      </div>
+      <hr />
+      <div className="content3"> 
+        <h3>SMK Pelayaran Dewaruci Jakarta</h3>
+        <p><i className="fa fa-calendar fa-fw large" />2016-2019</p>
+        <p>SMK Pelayaran Dewaruci Jakarta dengan nilai kelulusan 21.0 , jurusan Nautika Kapal Niaga
+        </p>
+      </div>
+    </div>
+  </div>
+  <div className="padding-bottom">
+    <div className="bottom">
+      <div className="content1">
+        <h2><i className="fa fa-briefcase fa-fw large" />Pengalaman :</h2>
+        <h3>Google Data Analytics Professional Certificate</h3>
+        <p><i className="fa fa-calendar fa-fw large" />2020</p>
+      </div>
+      <hr />
+      <div className="content2">
+        <h3>Microsoft Certified: Power BI Data Analyst Associate</h3>
+        <p><i className="fa fa-calendar fa-fw large" />2021</p>
+      </div>
+      <hr />
+      <div className="content3"> 
+        <h3>IBM Data Analyst Professional Certificate</h3>
+        <p><i className="fa fa-calendar fa-fw large" />2022</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
   );
 }
 
