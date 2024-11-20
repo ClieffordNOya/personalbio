0faff32c19ce869d5686033af83dca395ae46251
ClieffordNOya <clieffordnoya28@gmail.com>
2024-11-20 14:34:29 +0800

    adding side_Right

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import Side_Left from "./Commponents/side_left";
+import Side_Right from "./Commponents/side_right";
 
 const App = () => {
   return (
 <div className="container">
   <Side_Left />
-  <div className="container-padding">
-    <div className="padding">
-      <div className="content1">
-        <h2><i className="fa fa-briefcase fa-fw large" />School</h2>
-        <h3>SD Advent Salemba Jakarta</h3>
-        <p><i className="fa fa-calendar fa-fw large" />2008-2013</p>
-        <p>SD Advent Salemba Jakarta dengan nilai kelulusan 25.60
-        </p>
-      </div>
-      <hr />
-      <div className="content2">
-        <h3>SMP Advent Salemba Jakarta</h3>
-        <p><i className="fa fa-calendar fa-fw large" />2013-2016</p>
-        <p>SMP Advent Salemba Jakarta dengan nilai kelulusan 29.0
-        </p>
-      </div>
-      <hr />
-      <div className="content3"> 
-        <h3>SMK Pelayaran Dewaruci Jakarta</h3>
-        <p><i className="fa fa-calendar fa-fw large" />2016-2019</p>
-        <p>SMK Pelayaran Dewaruci Jakarta dengan nilai kelulusan 21.0 , jurusan Nautika Kapal Niaga
-        </p>
-      </div>
-    </div>
-  </div>
+  <Side_Right />
   <div className="padding-bottom">
     <div className="bottom">
       <div className="content1">
