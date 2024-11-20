ace691c974ba73da28686afff01b3d786bd2091e
ClieffordNOya <clieffordnoya28@gmail.com>
2024-11-20 14:36:45 +0800

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./Commponents/footer";
 import Side_Left from "./Commponents/side_left";
 import Side_Right from "./Commponents/side_right";
 
@@ -6,25 +7,7 @@ const App = () => {
 <div className="container">
   <Side_Left />
   <Side_Right />
-  <div className="padding-bottom">
-    <div className="bottom">
-      <div className="content1">
-        <h2><i className="fa fa-briefcase fa-fw large" />Pengalaman :</h2>
-        <h3>Google Data Analytics Professional Certificate</h3>
-        <p><i className="fa fa-calendar fa-fw large" />2020</p>
-      </div>
-      <hr />
-      <div className="content2">
-        <h3>Microsoft Certified: Power BI Data Analyst Associate</h3>
-        <p><i className="fa fa-calendar fa-fw large" />2021</p>
-      </div>
-      <hr />
-      <div className="content3"> 
-        <h3>IBM Data Analyst Professional Certificate</h3>
-        <p><i className="fa fa-calendar fa-fw large" />2022</p>
-      </div>
-    </div>
-  </div>
+  <Footer />
 </div>
 
 
---
+++ src/Commponents/footer/index.js
@@ -0,0 +1,25 @@
+const Footer = () =>{
+    return(
+        <div className="padding-bottom">
+    <div className="bottom">
+      <div className="content1">
+        <h2><i className="fa fa-briefcase fa-fw large" />Pengalaman :</h2>
+        <h3>Google Data Analytics Professional Certificate</h3>
+        <p><i className="fa fa-calendar fa-fw large" />2020</p>
+      </div>
+      <hr />
+      <div className="content2">
+        <h3>Microsoft Certified: Power BI Data Analyst Associate</h3>
+        <p><i className="fa fa-calendar fa-fw large" />2021</p>
+      </div>
+      <hr />
+      <div className="content3"> 
+        <h3>IBM Data Analyst Professional Certificate</h3>
+        <p><i className="fa fa-calendar fa-fw large" />2022</p>
+      </div>
+    </div>
+  </div>
+    )
+}
+
+export default Footer
