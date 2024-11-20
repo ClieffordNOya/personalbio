acaae322d6eda0506d7bce6a25f70f83020fd8e3
ClieffordNOya <clieffordnoya28@gmail.com>
2024-11-20 14:49:07 +0800

    adding database connection

---
+++ src/Config/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7lza815LMfstnOGF8--BlbCGrIOIPB38",
+  authDomain: "personalbio-400cd.firebaseapp.com",
+  databaseURL: "https://personalbio-400cd-default-rtdb.firebaseio.com",
+  projectId: "personalbio-400cd",
+  storageBucket: "personalbio-400cd.firebasestorage.app",
+  messagingSenderId: "557967942839",
+  appId: "1:557967942839:web:3dce9f9b9aab65f7579932"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
