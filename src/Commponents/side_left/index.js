2421f2e530a9edb2e37f3cdc50da58a59bec31ed
ClieffordNOya <clieffordnoya28@gmail.com>
2024-11-20 14:25:17 +0800

    adding side_life

---
+++ src/Commponents/side_left/index.js
@@ -0,0 +1,50 @@
+const Side_Left = () =>{
+    return (
+        <div id="colspan">
+    <img src="image/fotosaya.jpg" className="img1" />
+    <h4>CLIEFFORD NOYA</h4>
+    <p><i className="fa fa-briefcase fa-fw large" />Web Developer</p>
+    <p><i className="fa fa-home fa-fw large" />Minahasa Utara, INDONESIA</p>
+    <p><i className="fa fa-phone fa-fw large" />089686272363</p>
+    <hr />
+    <h3><i className="fa fa-linux fa-fw large" />Skills</h3>
+    <p>HTML</p>
+    <div className="load">
+      <div className="progress" style={{width: '90%'}}>90%</div>
+    </div>
+    <p>CSS</p>
+    <div className="load2">
+      <div className="progress2" style={{width: '80%'}}>80%</div>
+    </div>
+    <p>Java Script</p>
+    <div className="load3">
+      <div className="progress3" style={{width: '70%'}}>70%</div>
+    </div>
+    <p>Go Lang</p>
+    <div className="load4">
+      <div className="progress4" style={{width: '65%'}}>65%</div>
+    </div>
+    <p>Adobe Photoshop</p>
+    <div className="load4">
+      <div className="progress4" style={{width: '65%'}}>65%</div>
+    </div>
+    <p>Adobe Premiere</p>
+    <div className="load4">
+      <div className="progress4" style={{width: '65%'}}>65%</div>
+    </div>
+    <hr />
+    <h3><i className="fa fa-language fa-fw large" />Language</h3>
+    <p>English</p>
+    <p>Japanese</p>
+    <hr />
+    <h3><i className="fa fa-globe fa-fw large" />Social Media</h3>
+    <a href="#"><i className="fa fa-facebook fa-fw large" /></a>
+    <a href="#"><i className="fa fa-instagram fa-fw large" /></a>
+    <a href="#"><i className="fa fa-twitter fa-fw large" /></a>
+    <a href="#"><i className="fa fa-github fa-fw large" /></a>
+    <a href="#"><i className="fa fa-git fa-fw large" /></a>
+  </div>
+    )
+}
+
+export default Side_Left
